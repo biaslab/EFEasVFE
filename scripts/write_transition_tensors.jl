abb45267970c450b500aa67e49cc20ba8637e956
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2025-03-27 21:01:25 +0100

    fix: commit key_door not door_key

--- scripts/write_transition_tensors.jl
+++ scripts/write_transition_tensors.jl
@@ -7,7 +7,7 @@ include(srcdir("environments", "minigrid.jl"))
 function write_all_tensors(grid_size)
 
     observation_tensors = generate_observation_tensor(grid_size)
-    door_key_transition_tensor = get_door_key_state_transition_tensor(grid_size)
+    door_key_transition_tensor = get_key_door_state_transition_tensor(grid_size)
     orientation_transition_tensor = get_orientation_transition_tensor()
     location_transition_tensor = get_self_transition_tensor(grid_size)
 
@@ -23,7 +23,8 @@ function write_all_tensors(grid_size)
     npzwrite("data/raw_tensors/grid_size$(grid_size)/orientation_transition_tensor.npy", orientation_transition_tensor)
     npzwrite("data/raw_tensors/grid_size$(grid_size)/location_transition_tensor.npy", location_transition_tensor)
 end
-grid_size = 4
+
+grid_size = 5
 
 write_all_tensors(grid_size)
 
